081d32aa0d654851536337b9fd66ca074fa32368
Studdnet <st.codding@gmail.com>
2023-01-19 22:39:10 +0300

    feat: script for {input} is completed

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-"use strict"; 
+"use strict";
+
+const wrapper = document.querySelector(".wrapper"),
+	searchInput = wrapper.querySelector("input"),
+	infoText = wrapper.querySelector(".info-text");
+
+//fetch api function
+function fetchApi(word) {
+	infoText.style.color = "#000";
+	infoText.innerHTML = `Searching the meaning of <span>"${word}"</span>`;
+	let url = ``;
+}
+
+searchInput.addEventListener("keydown", (e) => {
+	if (e.key === "Enter" && e.target.value) {
+		fetchApi(e.target.value);
+	}
+});
